eca88cc0df24ac3e7da1ddc24b8dbece1b9eab6e
Prashant-Acharya <pacharya97.pa@gmail.com>
2019-05-16 15:52:07 +0545

    Favourite & Trending Page Created

--- src/Components/Favourites/Favourite.jsx
+++ src/Components/Favourites/Favourite.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import Layout from '../Layout/Layout'
+import movieData from '../../fakemoviedata'
+import MovieCard from '../MovieCard/MovieCard'
 
 class Favourite extends Component {
-	state = {  }
+	state = { movies: [] }
+
+	componentDidMount(){
+		const movies = [...movieData]
+		this.setState({
+			movies
+		})
+	}
+
 	render() { 
 		return (
 			<Layout>
-				<h1>Favourites</h1>
+				<h3 className='title'>Favourites</h3>
+				<div>
+					<div className="row">
+						{this.state.movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
+					</div>
+				</div>
 			</Layout>
 		 )
 	}
